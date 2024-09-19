1f3562649c557d88decba34ed6b2147960a43e1f
Katoga <katoga.cz@hotmail.com>
2024-09-19 12:08:37 +0200

    Parse dummy response JSON

--- internal/collector/main.go
+++ internal/collector/main.go
@@ -1,10 +1,18 @@
 package collector
 
 import (
-	"math/rand/v2"
+	"encoding/json"
+	"log"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
+
+type responseItem struct {
+	Ping     float64 `json:"ping"`
+	Jitter   float64 `json:"jitter"`
+	Upload   float64 `json:"upload"`
+	Download float64 `json:"download"`
+}
 
 type results struct {
 	Upload   float64
@@ -75,10 +83,55 @@ func (c *collector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c *collector) getResults() results {
-	return results{
-		Upload:   rand.Float64() * 100,
-		Download: rand.Float64() * 100,
-		Ping:     rand.Float64() * 10,
-		Jitter:   rand.Float64() * 10,
+	content, errDownload := c.download()
+	if errDownload != nil {
+		log.Panicf("Getting libresped data failed: %s", errDownload)
 	}
+
+	response := []responseItem{}
+	errJson := json.Unmarshal(content, &response)
+	if errJson != nil {
+		log.Panicf("Parsing JSON failed: %s", errJson)
+	}
+
+	res := response[0]
+
+	return results{
+		Upload:   res.Upload,
+		Download: res.Download,
+		Ping:     res.Ping,
+		Jitter:   res.Jitter,
+	}
+}
+
+func (c *collector) download() ([]byte, error) {
+	data := `[
+  {
+    "timestamp": "2024-09-18T20:56:48.944852891Z",
+    "server": {
+      "name": "Prague, Czech Republic (Turris)",
+      "url": "http://librespeed.turris.cz"
+    },
+    "client": {
+      "ip": "",
+      "hostname": "",
+      "city": "",
+      "region": "",
+      "country": "",
+      "loc": "",
+      "org": "",
+      "postal": "",
+      "timezone": ""
+    },
+    "bytes_sent": 182681600,
+    "bytes_received": 193406445,
+    "ping": 5,
+    "jitter": 2.37,
+    "upload": 93.67,
+    "download": 99.17,
+    "share": ""
+  }
+]
+`
+	return []byte(data), nil
 }
